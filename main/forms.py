0717cb43ac533337f5094f8b1081b1a3166437f4
kelechi chukwu <leochukwu48@gmail.com>
2024-01-22 15:17:34 +0100

    initial commit

--- main/forms.py
+++ main/forms.py
@@ -6,3 +6,12 @@ class MemberForms(forms.ModelForm):
     class Meta:
         model = Member
         fields = ['name','number','location','age','quote','location']
+
+        widgets ={
+            'name': forms.TextInput(attrs={'class':'form-control','placeholder':'Enter Your name'}),
+            'location': forms.TextInput(attrs={'class':'form-control','placeholder':'Enter Your Location'}),
+            'number': forms.TextInput(attrs={'class':'form-control','placeholder':'Enter Your Number'}),
+            'age': forms.TextInput(attrs={'placeholder':'Enter Your Age','class':'form-control'}),
+            'quote': forms.Textarea(attrs={'class':'form-control','placeholder':'Enter Your Favourites Quote'}),
+
+        }
