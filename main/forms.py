8ea9f3d65c9fe026b14a35570e357814b9c4f207
kelechi chukwu <leochukwu48@gmail.com>
2024-02-01 18:34:49 +0100

    initial commit

--- main/forms.py
+++ main/forms.py
@@ -8,15 +8,16 @@ from django.contrib.auth.models import User
 class MemberForms(forms.ModelForm):
     class Meta:
         model = Member
-        fields = ['name','number','location','age','quote','location']
+        fields = ['name','gender','number','location','age','quote','location',]
 
         widgets ={
             'name': forms.TextInput(attrs={'class':'form-control','placeholder':'Enter Your name'}),
+            'gender': forms.TextInput(attrs={'class':'form-control','placeholder':'Enter Your Gender'}),
             'location': forms.TextInput(attrs={'class':'form-control','placeholder':'Enter Your Location'}),
             'number': forms.TextInput(attrs={'class':'form-control','placeholder':'Enter Your Number'}),
             'age': forms.TextInput(attrs={'placeholder':'Enter Your Age','class':'form-control'}),
             'quote': forms.Textarea(attrs={'class':'form-control','placeholder':'Enter Your Favourites Quote'}),
-
+           
         }
 
 
